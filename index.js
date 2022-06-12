e83b97c40b63db9e61db1d7d9282b69e1b31375c
Anne-Stine Gjervoldstad <annestine.kodehode@gmail.com>
2022-06-12 20:48:21 +0200

    Update
    New design and cleaner code

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
-//#region --- Metric/Imperial Unit Conversion
+const inputEl = document.querySelector("#input-el")
+const convertBtn = document.querySelector("#convert-btn")
+const lengthEl = document.querySelector("#length-el")
+const volumeEl = document.querySelector("#volume-el")
+const massEl = document.querySelector("#mass-el")
 
-// Grab all elements
-let lengthEl = document.getElementById("length-el")
-let volumeEl = document.getElementById("volume-el")
-let massEl = document.getElementById("mass-el")
-
-
-// Conversions triggered oninput
-
-function convert(valNum) {
+const convert = (valNum) =>
+{
+    if (!valNum){valNum = 0}
 
     // Convert length
     feet = valNum * 3.281
     meters = valNum / 3.281
-    lengthEl.innerText = valNum + " meters" + " = " + feet.toFixed(3) + " feet" + " | " + valNum + " feet" + " = " + meters.toFixed(3) + " meters"
+    lengthEl.innerText = `${valNum} meters = ${feet.toFixed(3)} feet | ${valNum} feet = ${meters.toFixed(3)} meters`
     
     // Convert volume
     gallons = valNum * 0.264
     liters = valNum / 0.264
-    volumeEl.innerText = valNum + " liters" + " = " + gallons.toFixed(3) + " gallons" + " | " + valNum + " gallons" + " = " + liters.toFixed(3) + " liters"
+    volumeEl.innerText = `${valNum} liters = ${gallons.toFixed(3)} gallons | ${valNum} gallons = ${liters.toFixed(3)} liters`
     
     // Convert mass
     pounds = valNum * 2.205
     kilos = valNum / 2.205
-    massEl.innerText = valNum + " kilos" + " = " + pounds.toFixed(3) + " pounds" + " | " + valNum + " pounds" + " = " + kilos.toFixed(3) + " kilos"
-
-    // Let value be 0 if input is empty
-    if (valNum == "") {
-        valNum = 0
-    }
-
-    // Change plural words into singular words if value is 1
-    if (valNum == 1) {
-        
-    }
+    massEl.innerText = `${valNum} kilos = ${feet.toFixed(3)} pounds | ${valNum} pounds = ${kilos.toFixed(3)} kilos`
 }
 
-//#endregion
+convertBtn.addEventListener("click", () => convert(inputEl.value))
+document.addEventListener("keydown", (e) => {if (e.key === "Enter"){convert(inputEl.value)}})
